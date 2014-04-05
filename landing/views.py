8409fcbd13a4218d5952ed99d8942e4e9164e666
Domen <domen.krzisnik@klika.si>
2014-04-06 00:32:10 +0200

    subscriptions

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+import mailchimp
+
 # Create your views here.
 def home(request):
 	return render(request, 'index.html', {})
+
+def thanks(request):
+	return render(request, 'thanks.html', {})
+
+def contact(request):
+    if request.method == 'POST': # If the form has been submitted...
+    	try:
+    		list = mailchimp.utils.get_connection().get_list_by_id('ad3586ef91')
+    		email = request.POST['sender']
+    		list.subscribe(email, {'EMAIL':email}, double_optin=False)
+    	except:
+    		pass
+
+        return HttpResponseRedirect('/thanks') # Redirect after POST
+    else:
+    	return render(request, 'index.html', {})
--- startupweekend/urls.py
+++ startupweekend/urls.py
@@ -6,6 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'landing.views.home', name='home'),
+    url(r'^sendContact$', 'landing.views.contact', name='home'),
+    url(r'^thanks$', 'landing.views.thanks', name='thanks'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
