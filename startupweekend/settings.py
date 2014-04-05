f5ac7027ca46d1c9ed81540d1f3671a588d0d81d
Fibiola <natasha.pristovsek@gmail.com>
2014-04-05 18:34:25 +0200

    landing

--- startupweekend/settings.py
+++ startupweekend/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,8 @@ WSGI_APPLICATION = 'startupweekend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.pg',
+        'NAME': os.path.join(BASE_DIR, 'db.pg'),
     }
 }
 
